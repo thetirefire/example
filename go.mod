2a508d302cacdf9ee4ef42383f62e968fc8837ec
Jason DeTiberus <detiber@users.noreply.github.com>
2021-04-03 18:18:08 -0400

    finish wiring things together

--- go.mod
+++ go.mod
@@ -5,9 +5,11 @@ go 1.16
 require (
 	github.com/onsi/ginkgo v1.15.0
 	github.com/onsi/gomega v1.10.5
-	github.com/robfig/cron v1.2.0
-	k8s.io/api v0.21.0-rc.0
-	k8s.io/apimachinery v0.21.0-rc.0
-	k8s.io/client-go v0.21.0-rc.0
+	github.com/thetirefire/badidea v0.0.0-20210403205638-26f5cc7e55b6
+	k8s.io/apimachinery v0.21.0-beta.1
+	k8s.io/client-go v0.21.0-beta.1
+	k8s.io/klog/v2 v2.8.0
 	sigs.k8s.io/controller-runtime v0.9.0-alpha.1
 )
+
+replace github.com/googleapis/gnostic => github.com/googleapis/gnostic v0.4.1
--- main.go
+++ main.go
@@ -19,16 +19,23 @@ package main
 import (
 	"context"
 	"flag"
+	"io"
 	"os"
+	"os/exec"
+	"time"
 
-	"k8s.io/apimachinery/pkg/runtime"
-	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
-	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
-	ctrl "sigs.k8s.io/controller-runtime"
-	"sigs.k8s.io/controller-runtime/pkg/log/zap"
-
+	"github.com/thetirefire/badidea/server"
 	foov1 "github.com/thetirefire/example/api/v1"
 	"github.com/thetirefire/example/controllers"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/util/wait"
+	"k8s.io/client-go/discovery"
+	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
+	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
+	"k8s.io/client-go/tools/clientcmd"
+	clientcmdapi "k8s.io/client-go/tools/clientcmd/api"
+	"k8s.io/klog/v2/klogr"
+	ctrl "sigs.k8s.io/controller-runtime"
 	// +kubebuilder:scaffold:imports
 )
 
@@ -44,6 +51,28 @@ func init() {
 	// +kubebuilder:scaffold:scheme
 }
 
+func badIdeaClientConfig() clientcmd.ClientConfig {
+	clustercfg := clientcmdapi.NewCluster()
+	clustercfg.InsecureSkipTLSVerify = true
+	clustercfg.Server = "https://localhost:6443"
+
+	usercfg := clientcmdapi.NewAuthInfo()
+	usercfg.Username = "bad"
+	usercfg.Password = "idea"
+
+	contextcfg := clientcmdapi.NewContext()
+	contextcfg.AuthInfo = "badidea"
+	contextcfg.Cluster = "badidea"
+
+	apiConfig := clientcmdapi.NewConfig()
+	apiConfig.Clusters["badidea"] = clustercfg
+	apiConfig.AuthInfos["badidea"] = usercfg
+	apiConfig.Contexts["badidea"] = contextcfg
+	apiConfig.CurrentContext = "badidea"
+
+	return clientcmd.NewDefaultClientConfig(*apiConfig, nil)
+}
+
 func main() {
 	var metricsAddr string
 	var enableLeaderElection bool
@@ -53,9 +82,82 @@ func main() {
 			"Enabling this will ensure there is only one active controller manager.")
 	flag.Parse()
 
-	ctrl.SetLogger(zap.New(zap.UseDevMode(true)))
+	ctrl.SetLogger(klogr.New())
 
-	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
+	sigHandler := ctrl.SetupSignalHandler()
+	go func() {
+		if err := server.RunBadIdeaServer(sigHandler.Done()); err != nil {
+			setupLog.Error(err, "problem running manager")
+			os.Exit(1)
+		}
+	}()
+
+	clientConfig := badIdeaClientConfig()
+
+	restConfig, err := clientConfig.ClientConfig()
+	if err != nil {
+		setupLog.Error(err, "failed to create badidea client")
+		os.Exit(1)
+	}
+
+	pollContext, cancel := context.WithTimeout(sigHandler, 30*time.Second)
+	defer cancel()
+	if err := wait.PollUntil(
+		1*time.Second,
+		func() (bool, error) {
+			setupLog.Info("attempting to talk to badidea apiserver")
+			dc, err := discovery.NewDiscoveryClientForConfig(restConfig)
+			if err != nil {
+				setupLog.Error(err, "failed to create discovery client")
+				return false, nil
+			}
+			verInfo, err := dc.ServerVersion()
+			if err != nil {
+				setupLog.Error(err, "failed to query server version")
+				return false, nil
+			}
+
+			setupLog.Info("server responded", "version", verInfo)
+			return true, nil
+		},
+		pollContext.Done(),
+	); err != nil {
+		setupLog.Error(err, "timed out attempting to create badidea client")
+	}
+
+	// TODO: deploy crd resources
+	c1 := exec.Command("kustomize", "build", "config/crd")
+	c2 := exec.Command("kubectl", "--server", "https://localhost:6443", "--insecure-skip-tls-verify", "--username", "bad", "--password", "idea", "create", "-f", "-")
+
+	r, w := io.Pipe()
+	c1.Stdout = w
+	c2.Stdin = r
+	c2.Stdout = os.Stdout
+
+	if err := c1.Start(); err != nil {
+		setupLog.Error(err, "failed to start kustomize")
+		os.Exit(1)
+	}
+
+	if err := c2.Start(); err != nil {
+		setupLog.Error(err, "failed to start kubectl")
+		os.Exit(1)
+	}
+
+	go func() {
+		defer w.Close()
+		if err := c1.Wait(); err != nil {
+			setupLog.Error(err, "failed to run kustomize")
+			os.Exit(1)
+		}
+	}()
+
+	if err := c2.Wait(); err != nil {
+		setupLog.Error(err, "failed to run kubectl")
+		os.Exit(1)
+	}
+
+	mgr, err := ctrl.NewManager(restConfig, ctrl.Options{
 		Scheme:             scheme,
 		MetricsBindAddress: metricsAddr,
 		Port:               9443,
@@ -74,7 +176,7 @@ func main() {
 	// +kubebuilder:scaffold:builder
 
 	setupLog.Info("starting manager")
-	if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil {
+	if err := mgr.Start(sigHandler); err != nil {
 		setupLog.Error(err, "problem running manager")
 		os.Exit(1)
 	}
